8043b84ae7702cc529294890fb9e4ff408978ede
EdsonZuchi <edsonluiz.zuchi@gmail.com>
2024-02-16 23:55:18 -0300

    main 2 and 3

--- listaPonteiros/main2.c
+++ listaPonteiros/main2.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+
 int main(){
+
+    int v2;
+    int v1;
+
+    if(&v1 > &v2){
+        printf("Endereço maior: %p da v1", &v1);
+    }else{
+        printf("Endereço maior: %p da v2", &v2);
+    }
+    
     return 0;
 }
